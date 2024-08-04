e6e7a183b2f5381c57752f1081d4a36ba056b31f
Piotr Kulesza <139692181+p-kulesza@users.noreply.github.com>
2024-08-04 20:49:49 +0200

    trial

--- src/test/java/RestfulAPITest.java
+++ src/test/java/RestfulAPITest.java
@@ -1,11 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeAll;
 import org.testng.annotations.Test;
-
-import java.net.StandardSocketOptions;
-
-import static io.restassured.RestAssured.given;
 
 public class RestfulAPITest {
     private static final String BASE_URL = "https://reqres.in/api";
