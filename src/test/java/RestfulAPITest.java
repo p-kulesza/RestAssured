e9a0d0d22f799525c0a7210f09ab9dd6c9b86bf8
Piotr Kulesza <139692181+p-kulesza@users.noreply.github.com>
2024-08-04 16:24:49 +0200

    trial

--- src/test/java/RestfulAPITest.java
+++ src/test/java/RestfulAPITest.java
@@ -2,10 +2,10 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-public class RestTest {
+public class RestfulAPITest {
     @Test
     public void test_1(){
-        Response response = RestAssured.get("https://mvnrepository.com/");
+        Response response = RestAssured.get("https://dummyapi.io/");
         int statusCode = response.getStatusCode();
         long responseTime = response.getTime();
         System.out.println("Status code is: " + statusCode);
